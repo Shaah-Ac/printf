fdc166567757de082c09fbab95ae092dba90a331
Sharon Achieng <otienosharon51@gmail.com>
2023-10-16 10:38:53 -0700

    printf team project

--- 10-precision.c
+++ 10-precision.c
@@ -1,17 +1,8 @@
-#include "main.h"
-
-/**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
- */
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
 	int precision = -1;
+	va_list copy;
 
 	if (format[curr_i] != '.')
 		return (precision);
@@ -28,7 +19,9 @@ int get_precision(const char *format, int *i, va_list list)
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
-			precision = va_arg(list, int);
+			va_copy(copy, list);
+			precision = va_arg(copy, int);
+			va_end(copy);
 			break;
 		}
 	}
